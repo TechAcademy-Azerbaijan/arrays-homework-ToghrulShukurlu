6a1002803af932370a02ea5ac618e0e17f2da939
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-06-28 18:42:09 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -3,7 +3,19 @@ prompt.start();
 
 
 prompt.get('input', function (err, result) {
-  
-    // Write code here
+      arr = arr.split(" ").map(Number);
+    let count = 0
+    while (arr.length > 0) {
+        count++
+        let x = arr[0]
+        arr.unshift()
+        console.log(arr);
+        while (arr.includes(x)) {
+            index = arr.indexOf(x)
+            arr.splice(index, 1)
+        }
+    }
+    console.log(count);
+
   
 });
