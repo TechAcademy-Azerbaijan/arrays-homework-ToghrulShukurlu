ce183cb3e6d08e3f01e1e9d55a38ccd2168ff12f
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-06-28 18:56:02 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let arr2 = []
+for (let index = 0; index < arr.length; index++) {
+    if (index % 2 != 1) {
+        arr2.push(arr[index])
+    }
+}
+console.log(...arr2);
   
 });
